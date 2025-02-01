1d7eac621fc45a1eea695d54f668c8e9413470f4
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-02-01 15:13:00 +0100

    blabla

--- repo/ex03/main.cpp
+++ repo/ex03/main.cpp
@@ -4,25 +4,35 @@
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 
-static void test(const Point& a, const Point& b, const Point& c, const Point& p)
+static void testPoint(const Point& a, const Point& b, const Point& c, const Point& p)
 {
 	if (bsp(a, b, c, p))
-		std::cout << GREEN << p << " is inside ABC" << RESET << std::endl;
+		std::cout << GREEN << p << " is strictly inside ABC" << RESET << std::endl;
 	else
-		std::cout << RED << p << " is outside ABC" << RESET << std::endl;
+		std::cout << RED << p << " is outside ABC (or on its boundary)" << RESET << std::endl;
 }
 
 int main()
 {
-	const Point a(0, 0);
-	const Point b(1, 0);
-	const Point c(1, 1);
+	const Point a(-1.24, 0.52);
+	const Point b(4.7, 3.26);
+	const Point c(1.28, -3.04);
 
 	std::cout << "A = " << a << std::endl;
 	std::cout << "B = " << b << std::endl;
 	std::cout << "C = " << c << std::endl;
 
-	test(a, b, c, Point(3, 3));
-	test(a, b, c, Point(0.5, 0.5));
-	test(a, b, c, Point(0.5, 0.25));
+	/* inside */
+	testPoint(a, b, c, Point(2.5, 1.56));
+	testPoint(a, b, c, Point(1.58, -2.12));
+	testPoint(a, b, c, Point(-0.18, 0.7));
+
+	/* outside */
+	testPoint(a, b, c, Point(3, 7));
+	testPoint(a, b, c, Point(5.001, -0.26));
+	testPoint(a, b, c, Point(-0.22, -1.64));
+
+	/* boundary */
+	testPoint(a, b, c, Point(4.7, 3.26));
+	testPoint(Point(0, 0), Point(1, 0), Point(1, 1), Point(0.5, 0.5));
 }
